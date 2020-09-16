378f98ebabe341726526ab60ca9bf14f0e5a9ba0
tuntuntuntuntun <tuntuntun02b@gmail.com>
2020-09-16 19:08:37 +0900

    黒白交互に石を置く処理

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+let color = 'black'
+
+// 石の色を変える
+const checkColor = (color) => {
+    if (color == 'black') {
+        return (color = 'white')
+    } else {
+        return (color = 'black')
+    }
+}
+
 window.onload = () => {
+
     // 石を置く
     Array.from(document.getElementsByClassName('col')).forEach(element => {
         element.addEventListener('click', (e) => {
-            e.target.classList.add('black')
+            e.target.classList.add(color)
+            color = checkColor(color)
         })
     })
 }
