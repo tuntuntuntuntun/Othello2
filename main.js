94cbf44b8376387f7bf28c8bc25977cf158e7d7e
tuntuntuntuntun <tuntuntun02b@gmail.com>
2020-09-18 15:25:50 +0900

    全ての方向の石の色を変える処理

--- main.js
+++ main.js
@@ -51,6 +51,115 @@ const rightDisc = (row, col) => {
     return results
 }
 
+// 下方向の石を取得
+const downDisc = (row, col) => {
+    results = []
+    col = col.replace(/[^0-9]/g, '')
+    col = 'col' + col
+    
+    // 数字を抜き出す
+    let r = Number(row.replace(/[^0-9]/g, ''))
+    for (i = r + 1; i < 9; i++) {
+        row = 'row' + i
+        // row行目col列目の情報を取得
+        results.push(document.querySelector(`#${row} .${col}`))
+    }
+
+    return results
+}
+
+// 左方向の石を取得
+const leftDisc = (row, col) => {
+    results = []
+
+    // 数字を抜き出す
+    let c = Number(col.replace(/[^0-9]/g, ''))
+    for (i = c - 1; i > 0; i--) {
+        col = 'col' + i
+        // row行目col列目の情報を取得
+        results.push(document.querySelector(`#${row} .${col}`))
+    }
+
+    return results
+}
+
+// 右上方向の石を取得
+const upRightDisc = (row, col) => {
+    results = []
+
+    let r = Number(row.replace(/[^0-9]/g, ''))    
+    let c = Number(col.replace(/[^0-9]/g, ''))
+    while (r - 1 > 0 && c + 1 < 9) {
+        r--
+        c++
+
+        row = 'row' + r
+        col = 'col' + c
+
+        results.push(document.querySelector(`#${row} .${col}`))
+    }
+
+    return results
+}
+
+// 右下方向の石を取得
+const downRightDisc = (row, col) => {
+    results = []
+
+    let r = Number(row.replace(/[^0-9]/g, ''))    
+    let c = Number(col.replace(/[^0-9]/g, ''))
+    while (r + 1 < 9 && c + 1 < 9) {
+        r++
+        c++
+
+        row = 'row' + r
+        col = 'col' + c
+
+        results.push(document.querySelector(`#${row} .${col}`))
+    }
+
+    return results
+}
+
+// 左下方向の石を取得
+const downLeftDisc = (row, col) => {
+    results = []
+
+    let r = Number(row.replace(/[^0-9]/g, ''))    
+    let c = Number(col.replace(/[^0-9]/g, ''))
+    while (r + 1 < 9 && c - 1 > 0) {
+        r++
+        c--
+
+        row = 'row' + r
+        col = 'col' + c
+
+        results.push(document.querySelector(`#${row} .${col}`))
+    }
+
+    return results
+}
+
+// 左上方向の石を取得
+const upLeftDisc = (row, col) => {
+    results = []
+
+    let r = Number(row.replace(/[^0-9]/g, ''))    
+    let c = Number(col.replace(/[^0-9]/g, ''))
+    while (r - 1 > 0 && c - 1 > 0) {
+        r--
+        c--
+
+        row = 'row' + r
+        col = 'col' + c
+
+        results.push(document.querySelector(`#${row} .${col}`))
+    }
+
+    return results
+}
+
+// 石の色を変える
 function reverseDisc (results, color, enemyColor)
 {
     reverses = []
@@ -92,8 +201,20 @@ window.onload = () => {
             everyDirection = [
                 // 上方向の石を取得
                 upDisc(row, col),
-                // 右方向
-                rightDisc(row, col)
+                // 右
+                rightDisc(row, col),
+                // 下
+                downDisc(row, col),
+                // 左
+                leftDisc(row, col),
+                // 右上
+                upRightDisc(row, col),
+                // 右下
+                downRightDisc(row, col),
+                // 左下
+                downLeftDisc(row, col),
+                // 左上
+                upLeftDisc(row, col)
             ];
 
             for (result of everyDirection) {
